a180638efa1e2a6641da155d590c5ea6bf8edac9
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-01-06 21:20:21 +0300

    Task 4 correction

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+
+
+
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module"""
+    """Print all names defined by hidden_4 module."""
     import hidden_4
 
     names = dir(hidden_4)
     for nme in names:
-        if nme[:2] != "_":
+        if nme[:2] != "__":
             print(nme)
