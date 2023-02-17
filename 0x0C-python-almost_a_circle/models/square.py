68bc34aa7d1f59c8fb49c34fb8853abaf744afef
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-02-17 23:03:15 +0300

    Square.py update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,61 +1,68 @@
 #!/usr/bin/python3
-"""
-the class square that inherits from Rectangle
-"""
-from .rectangle import Rectangle
+"""This module contains a square class"""
+
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-        Class Square
-    """
+    """Represents a square"""
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        """
-        super().__init__(size, size, x, y, id)
         self.size = size
+        self.x = x
+        self.y = y
+        self.id = None
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Defines a format for the string representation of the class"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
 
     @property
     def size(self):
-        """
-            Size Getter
-        """
-        return self.width
+        """Gets the value of size"""
+        return self.__width
 
     @size.setter
     def size(self, value):
-        """
-            Size Setter
-        """
-        self.height = value
-        self.width = value
-
-    def __str__(self):
-        """informal string representation of the square"""
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
-                                                         self.y, self.width)
+        """Sets the value for size"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
 
     def update(self, *args, **kwargs):
-        """update"""
-        i = 0
-        if args:
-            for arg in args:
-                if i == 0:
-                    self.id = arg
-                if i == 1:
-                    self.size = arg
-                if i == 2:
-                    self.x = arg
-                if i == 3:
-                    self.y = arg
-                i += 1
+        """Updates attributes of an instance"""
+
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                if type(args[0]) != int and args[0] is not None:
+                    raise TypeError("id must be an integer")
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
         else:
-            for arg in kwargs:
-                setattr(self, arg, kwargs.get(arg))
+            for key, value in kwargs.items():
+                if key == "id":
+                    if type(value) != int and value is not None:
+                        raise TypeError("id must be an integer")
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
 
     def to_dictionary(self):
-        """
-            returns the dictionary
-            representation of a square
-        """
-        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+        """Returns the dictionary representation of a Square"""
+
+        obj_dictionary = {'id': self.id, 'size': self.size, 'x': self.x,
+                          'y': self.y}
+
+        return obj_dictionary
