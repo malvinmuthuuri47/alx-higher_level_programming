197d537ae085208a22ed7590025d9c8f00a1cddd
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-03-15 21:17:48 +0300

    Task 14

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- This script computes the average of all records in the table of the Database
+-- in MYSQL server. The result column name should be average.
+SELECT AVG(score) AS average
+FROM second_table;
