681fd446b8dd0cc73ef430b6548d17258e88fbe6
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-04-04 15:09:13 +0300

    Task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- create Database and user
+-- give user only select privilege on the database
+-- if db or user already exists, script shouldn't fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
