3bcf97db28346c57d637977316de560bba940df0
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-04-04 15:11:34 +0300

    Task 2 correction

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,5 @@
 -- if db or user already exists, script shouldn't fail
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
