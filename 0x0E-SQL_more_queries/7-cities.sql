76ad61f8aee460c15ad80ba51ec5a20e417d769d
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-04-04 18:55:01 +0300

    Task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- create a DB and adds a new table in the db
+-- if db/table already exists, script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES states(id)
+	);
