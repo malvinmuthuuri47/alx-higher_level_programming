cc10e3874d5cb667fc9591246f222d4f5d39752e
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-04-04 15:37:34 +0300

    Task 4

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,8 @@
+-- create a table on the sql server
+-- Db name will be passed as an argument in the sql command
+-- if table exists, the script shouldn't fail
+
+CREATE TABLE IF NOT EXISTS id_not_null(
+	id INT,
+	name VARCHAR(256)
+	);
