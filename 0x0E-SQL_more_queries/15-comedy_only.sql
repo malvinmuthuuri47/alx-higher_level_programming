c514ea1b8fd8f5684ad21f8890d983293d700ffc
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-04-05 13:50:40 +0300

    Task 15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- list all comedy shows in the database
+
+SELECT title FROM tv_genres
+	INNER JOIN tv_show_genres
+	ON tv_genres.id = tv_show_genres.genre_id
+	INNER JOIN tv_shows
+	ON tv_shows.id = tv_show_genres.show_id
+	WHERE name = 'Comedy'
+	ORDER BY title ASC;
