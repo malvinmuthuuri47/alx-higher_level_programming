227fd00886152d6d7721196ef542e423400a0992
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-05-01 22:26:34 +0300

    Task four

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import requests
+"""
+    This python script fetches a specified URL using the package requests
+"""
+
+
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
