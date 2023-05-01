a4ae550031fd6e8a356145121b6046e963628527
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-05-01 23:11:53 +0300

    Task four correction

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,8 +3,7 @@ import requests
 """This script fetches from a URL using the package requests"""
 
 
-if __name__ == "__main__":
-    response = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body response:")
-    print(f"\t- type: {type(response.text)}")
-    print(f"\t- content: {response.text}")
+response = requests.get("https://alx-intranet.hbtn.io/status")
+print("Body response:")
+print(f"\t- type: {type(response.text)}")
+print(f"\t- content: {response.text}")
