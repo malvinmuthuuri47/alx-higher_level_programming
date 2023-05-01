9f6c215b9aaee4ca0e708d77ee1a6a16b9c66767
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-05-01 16:45:46 +0300

    Task zero correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,11 @@
 """Fetch a url using urllib"""
 import urllib.request
 
+
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen('url') as response:
         content = response.read()
-
         print("Body response:")
         print(f"\t- type: {type(content)}")
         print(f"\t- content: {content}")
