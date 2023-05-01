2087abcae63faf076ad27e7a606dcb4e5cf63bd8
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-05-02 00:34:18 +0300

    Task 6 corection

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import sys
-import requests
 """
     This script takes in a URL and an email address, sends a POST request
     to the passed URL with the email as a parameter and finally displays
     the body of the response
 """
+import sys
+import requests
 
 
 if __name__ == "__main__":
