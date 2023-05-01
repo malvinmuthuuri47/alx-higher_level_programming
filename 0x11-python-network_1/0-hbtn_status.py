6a9a86ed9b1966f5f033b054c31267f508b6c952
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-05-01 16:40:11 +0300

    Task zero

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Fetch a url using urllib"""
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
+    as response:
+        content = response.read()
+
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {content.decode('utf-8')}")
