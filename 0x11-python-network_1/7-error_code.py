013ff39852b3b5527eade262f9fac84130f2ff6e
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-05-02 00:07:10 +0300

    Task seven

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+import requests
+"""
+    - This script takes in a URL, sends a request to the URL and displays
+      the body of the response.
+    - If the status code is greater than/equal to 400, print Error Code:
+      followed by the value of the HTTP status code.
+    - You are only allowed to use requests and sys packages
+"""
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = requests.get(url)
+
+    if req.status_code >= 400:
+        print(f"Error code: {req.status_code}")
+    else:
+        print(req.text)
