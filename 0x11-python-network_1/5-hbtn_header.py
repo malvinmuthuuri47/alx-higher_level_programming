977cddc5161c65e23c80422f3eba3f2430cc820b
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-05-02 00:20:17 +0300

    Task five correction

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import sys
-import requests
 """
     This script takes a URL, sends a request to that URL and displays
     the value of the variable provided in the response header
 """
+import sys
+import requests
 
 
 response = requests.get(sys.argv[1])
