a7b1b67ea2e1806ccb43bc4376aee508b89173b8
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-05-01 22:36:55 +0300

    Task four correction

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 import requests
-"""
-    This python script fetches a specified URL using the package requests
-"""
+"""This python script fetches a specified URL using the package requests"""
 
 
 if __name__ == "__main__":
