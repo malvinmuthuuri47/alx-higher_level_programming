4dde0e337827f9022dc3d238560b84418c55b0b3
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-05-24 22:50:46 +0300

    Task one

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filepath = process.argv[2];
+
+fs.readFile(filepath, { encoding: 'utf-8' }, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
