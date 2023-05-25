0345e601d713c96b3afb9aa28d903d27023e55e5
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-05-25 12:37:09 +0300

    Task six

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, ['uft-8'], (err, res, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  fs.writeFile(filePath, data, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
